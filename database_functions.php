95e728f938c4faf5a9fce41b8bafb6d305e99618
EC2 Default User <ec2-user@ip-172-31-11-17.us-west-2.compute.internal>
2017-04-17 21:17:37 +0000

    add assignment but this isn't done yet

--- database_functions.php
+++ database_functions.php
@@ -35,4 +35,28 @@ function exec_procedure($query, $bindings) {
 
 	oci_close($conn);
 }
+
+function upload_file($file, $path) {
+	$message = 'success';
+	if (!move_uploaded_file($file["tmp_name"], $path)) {
+		switch( $_FILES['file']['error'] ) {
+				case UPLOAD_ERR_OK:
+						break;
+				case UPLOAD_ERR_INI_SIZE:
+				case UPLOAD_ERR_FORM_SIZE:
+						$message = 'file too large';
+						break;
+				case UPLOAD_ERR_PARTIAL:
+						$message = 'file upload was not completed.';
+						break;
+				case UPLOAD_ERR_NO_FILE:
+						$message = 'zero-length file uploaded.';
+						break;
+				default:
+						$message = 'internal error #' . $file['error'];
+						break;
+		}
+	}
+	return($message);
+}
 ?>
