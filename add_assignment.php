216dbce0cbe92f5dc09eea631fb7bc7e5e7cae84
EC2 Default User <ec2-user@ip-172-31-11-17.us-west-2.compute.internal>
2017-04-17 19:23:55 +0000

    add grade, add assignment, get grades

--- add_assignment.php
+++ add_assignment.php
@@ -3,13 +3,15 @@
 	include 'db_login.php';
 
 	$due_date = null;
-	$query = "begin grader_pack.add_assignment(:crn, :assignment_name, :due_date); end;";
+	$query = "begin grader_pack.add_assignment(:crn, :assignment_name, :due_date, :outof, :weight); end;";
 
 	$stmt = oci_parse($conn, $query);
 
 	oci_bind_by_name($stmt, ':crn', $_POST['crn']);
 	oci_bind_by_name($stmt, ':assignment_name', $_POST['assignment_name']);
 	oci_bind_by_name($stmt, ':due_date', $due_date);
+	oci_bind_by_name($stmt, ':outof', $_POST['outof']);
+	oci_bind_by_name($stmt, ':weight', $_POST['weight']);
 
 	oci_execute($stmt);
 
