7e32a8c7ac661e0e40ac20fc25679e65c7d40cfd
Madelyn Nelson <mnelso12@nd.edu>
2017-05-02 06:10:34 +0000

    fml modals

--- scripts/main.js
+++ scripts/main.js
@@ -17,7 +17,7 @@ var repos = [];
 
 //stack for repo paths
 var repo_paths = [];
-var repo_id;
+var repo_id = "";
 
 var classes = [
 	{
@@ -284,7 +284,8 @@ function ready() {
 	});
 
 	// add instructor
-	$(document).on('click', "#addInstructorModalBtn", function() {
+	$("#addInstructorModalBtn").click( function() {
+		console.log("hi");
 
 		// get info from modal
 		var instructorInput = $("#instructorInput").val();
@@ -420,6 +421,7 @@ function fillInRepoViewerWithPath(path, back) {
 
 function fillInRepoViewer(id) {
 
+	repo_id = id;
 	$.post("GitGrader/php_scripts/get_directory_files.php", {repo_id : id},
 		function(data, status) {
 			console.log('here');
@@ -1033,7 +1035,7 @@ function modalButtonHandlers() {
 		// send new score to database via PHP
 		var ans = "";
 		
-		$.post("GitGrader/php_scripts/add_comment.php", {content: newComment, file_path: repo_paths[0], repo_id: repo_id},
+		$.post("GitGrader/php_scripts/add_comment.php", {content: newComment, file_path: repo_paths[repo_paths.length()-1], repo_id: repo_id},
 			function(data, status){
 				console.log(data, status);
 
