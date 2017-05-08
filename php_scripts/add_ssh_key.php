15a86e4c2371646ab6f2eb352b4a40bba70b1a68
Madelyn Nelson <mnelso12@nd.edu>
2017-05-08 00:33:06 +0000

    fixed ssh key

--- php_scripts/add_ssh_key.php
+++ php_scripts/add_ssh_key.php
@@ -3,7 +3,7 @@
 	include 'check_logged_in.php';
 
 	$authorized_keys = fopen('/home/ec2-user/.ssh/authorized_keys','a+');
-	fwrite($authorized_keys,$_POST["ssh_key"]);
+	fwrite($authorized_keys,$_POST["ssh_key"]."\n");
 	
 
 	
@@ -11,7 +11,7 @@
 
 	$stmt = oci_parse($conn, $query);
 	$stmt = oci_parse($conn, $query);
-	oci_bind_by_name($stmt, ':username', $_POST['username']);
+	oci_bind_by_name($stmt, ':username', $_SESSION['username']);
 	oci_bind_by_name($stmt, ':ssh_key', $_POST['ssh_key']);
 	oci_bind_by_name($stmt, ':ssh_title',$_POST['ssh_title']);
 	oci_execute($stmt);
